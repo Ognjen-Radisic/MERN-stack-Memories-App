2dd83fc8fa0c975ce675009c4b8d143a2cf22645
Ognjen Radisic <59142427+Ognjen-Radisic@users.noreply.github.com>
2020-12-14 21:18:18 +0100

    folder structure

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ import bodyPaser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+import postRoutes from './routes/posts.js'
+
 const app = express()
+
+//http://localhost:5000/posts
+app.use('/posts', postRoutes)
 
 app.use(bodyPaser.json({ limit: '30mb', extended: true }))
 app.use(bodyPaser.urlencoded({ limit: '30mb', extended: true }))
