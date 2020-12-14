7bee66261ac062d65c311df9f2096860398552e9
Ognjen Radisic <59142427+Ognjen-Radisic@users.noreply.github.com>
2020-12-14 20:29:43 +0100

    connecting database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,28 @@
 import express from 'express'
 import bodyPaser from 'body-parser'
 import mongoose from 'mongoose'
-import nodemon from 'nodemon'
+import cors from 'cors'
+
+const app = express()
+
+app.use(bodyPaser.json({ limit: '30mb', extended: true }))
+app.use(bodyPaser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+//https://www.mongodb.com/cloud/atlas
+const CONNECTION_URL =
+  'mongodb+srv://ognjen:<password>@cluster0.z7qkn.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
