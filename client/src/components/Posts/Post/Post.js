10665ad6da47d50caa9130bd4d77c75df847ffe8
Ognjen Radisic <59142427+Ognjen-Radisic@users.noreply.github.com>
2020-12-15 12:21:30 +0100

    jsx structure

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+      <h1>THhis is a post boys and girls</h1>
+    </div>
+  )
+}
+
+export default Post
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Post from './Post/Post.js'
+
+const Posts = () => {
+  return (
+    <div>
+      <h1>POSTS</h1>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
+
+export default Posts
