dcce1634a75ea46cdadd0a0930da1f2b50094501
Ognjen Radisic <59142427+Ognjen-Radisic@users.noreply.github.com>
2020-12-15 13:21:46 +0100

    styles

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,24 @@ import memories from './images/memories.png'
 import Posts from './components/Posts/Posts.js'
 import Form from './components/Form/Form.js'
 
+//Styles
+import useStyles from './styles.js'
+
 const App = () => {
+  const classes = useStyles()
+
   return (
     <Container maxWidth='lg'>
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>
           Memories
         </Typography>
-        <img src={memories} alt='memories' height='60' width='60' />
+        <img
+          className={classes.image}
+          src={memories}
+          alt='memories'
+          height='60'
+        />
       </AppBar>
       <Grow in>
         <Container>
